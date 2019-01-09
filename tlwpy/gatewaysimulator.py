598c6776f2a4882c600e19017a7804efedd460a0
Daniel Palmer <daniel@0x0f.com>
2019-01-09 20:44:06 +0900

    Closer to working integration with liblorawan

--- tlwpy/gatewaysimulator.py
+++ tlwpy/gatewaysimulator.py
@@ -1,6 +1,7 @@
 from tlwpy.mqttbase import MqttBase
 import json
 import tlwpy.liblorawan
+import base64
 
 PKTFWDBRROOT = 'pktfwdbr'
 RX_TYPE_JOIN = 'join'
@@ -21,13 +22,16 @@ class Gateway(MqttBase):
         self.mqtt_client.publish(topic, json.dumps(payload))
 
     async def join(self, app_eui: str, dev_eui: str):
+        assert len(app_eui) is 16
+        assert len(dev_eui) is 16
         topic = '%s/%s/rx/%s/%s/%s' % (PKTFWDBRROOT, self.__gateway_id, RX_TYPE_JOIN, app_eui, dev_eui)
 
-        data = tlwpy.liblorawan.builder_joinreq()
+        data = tlwpy.liblorawan.builder_joinreq(b'0000000000000000', bytes.fromhex(app_eui), bytes.fromhex(dev_eui),
+                                                b'00')
 
         payload = {"tmst": 3889331076, "chan": 1, "rfch": 0, "freq": 923.39999999999998, "stat": 1, "modu": "LORA",
                    "datr": "SF10BW125", "codr": "4/5", "lsnr": 12.0, "rssi": -48, "size": 23,
-                   "data": "AOpwANB+1bNwCFE2YzkxNjK10u5h9sE="}
+                   "data": base64.b64encode(data).decode('ascii')}
 
         await self.send_pktfwdbr_publish(topic, payload)
 
