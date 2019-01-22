53278fe9ec3732e967dc8b36492d35bca087c948
Daniel Palmer <daniel@0x0f.com>
2019-01-22 23:39:20 +0900

    Fix tests

--- tests/test_liblorawan.py
+++ tests/test_liblorawan.py
@@ -2,7 +2,7 @@ import tlwpy.liblorawan
 
 
 def test_build_joinreq():
-    packet = tlwpy.liblorawan.builder_joinreq(b'0000000000000000', b'00000000', b'00000000', b'00')
+    packet = tlwpy.liblorawan.build_joinreq(b'0000000000000000', b'00000000', b'00000000', 0)
     print(packet)
     assert type(packet) is bytearray
     assert len(packet) is 23
